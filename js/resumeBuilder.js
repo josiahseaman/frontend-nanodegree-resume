ddfeda058059502e0d72203e3f3a79d3a5c8de7d
Josiah Seaman <josiah.seaman@gmail.com>
2015-07-17 11:23:17 -0600

    Basic JSON objects with bare append() to modify DOM

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,34 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var bio = {
+    "name": "Josiah Seaman",
+    "role": "Developer",
+    "city": "Colorado Springs",
+    "age": 32,
+    "skills": ["CS", "Javascript", "Python", "GMing"]
+};
+
+$('#header').append(HTMLheaderName.replace('%data%', bio.name));
+$('#header').append(HTMLheaderRole.replace('%data%', bio.role));
+$('#header').append(HTMLskillsStart);
+for(var i in bio.skills){
+    $('#header').append(HTMLskills.replace('%data%', bio.skills[i]));
+}
+
+var work = {
+    "name": "Newline",
+    "position": "Lead Developer",
+    "city": "Colorado Springs"
+};
+$('#workExperience').append(work["position"]);
+
+var education = {
+    "name": "University of Colorado Anscutz Medical Campus",
+    "level": "PhD Program",
+    "degree": "Computational Biology"
+};
+$('#education').append(HTMLschoolStart);
+$('#education').append(HTMLschoolName.replace('%data%', education.name));
